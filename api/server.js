c0f6375a89e9accedab2763ebd4af26b4ea086c0
ChristoherHenao <cq.henao@gmail.com>
2021-08-30 17:00:55 -0400

    basic endpoints structure built out

--- api/server.js
+++ api/server.js
@@ -6,8 +6,29 @@ const server = express()
 
 server.use(express.json())
 
-server.get('/', (req, res) => {
+// Endpoints
+server.get('/api/users', (req, res) => {
     res.json({ message: 'hey there MP'})
+})
+
+server.get('/api/users/:id', (req, res) => {
+    res.json('the get ID is working')
+})
+
+server.post('/api/users', (req, res) => {
+    res.json('the post is working')
+})
+
+server.put('/api/users/:id', (req, res) => {
+    res.json('the put is working')
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    res.json('the delete is working')
+})
+
+server.get('*', (req, res) => {
+    res.status(404).json('this page does not exist')
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
